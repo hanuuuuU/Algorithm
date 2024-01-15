f30828c19afd8cbce65c8cc7cf10d55e22e6acbb
hanuuuuU <han980004@naver.com>
2024-01-15 23:57:38 +0900

    o

---
+++ BOJ/binary_search/2470.py
@@ -0,0 +1,21 @@
+n=int(input())
+liq=list(map(int,input().split()))
+liq.sort()
+mini=9999999999
+front=0
+end=n-1
+answer=[0,0]
+while front<=end:
+    s=liq[front]+liq[end]
+    if abs(s)<mini:
+        answer[0]=liq[front]
+        answer[1]=liq[end]
+        mini=abs(s)
+
+    if s<0:
+        front+=1
+    elif s>0:
+        end-=1
+    else:
+        break
+print(*answer)
