7fbf56983a5e222f823c210001fd3af7a9a8d96a
hanuuuuU <han980004@naver.com>
2024-01-23 01:48:06 +0900

    x

--- BOJ/binary_search/1253.py
+++ BOJ/binary_search/1253.py
@@ -1,2 +1,18 @@
 n=int(input())
 nums=sorted(list(map(int,input().split())))
+good=0
+
+for i in range(n):
+    goal=nums[i]
+    l=nums[:i]+nums[i+1:]
+    start,end=0,n-2
+    while start<end:
+        s=l[start]+l[end]
+        if s==goal:
+            good+=1
+            break
+        elif s>goal:
+            end-=1
+        else:
+            start+=1
+print(good)
