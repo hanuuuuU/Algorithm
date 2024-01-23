75018b9e9056e271771eee42959788dae0b201a3
hanuuuuU <han980004@naver.com>
2024-01-23 23:10:14 +0900

    o

---
+++ BOJ/binary_search/6236.py
@@ -0,0 +1,24 @@
+import sys
+input=sys.stdin.readline
+n,m=map(int,input().split())
+out=[int(input()) for _ in range(n)]
+
+start,end = max(out),sum(out)
+answer=0
+while start<=end:
+    mid=(start+end)//2
+    temp=0
+    cnt=1
+    for i in out:
+        if temp+i>mid:
+            temp=0
+            cnt+=1
+        temp+=i
+    if cnt<=m:
+        answer=mid
+        end=mid-1
+    else:
+        start=mid+1
+print(answer)
+
+# 2343과 같은 유형의 문제
