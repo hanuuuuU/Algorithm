15fb50874089761fbd8cbf05cea7ab394fdf8222
Jeongwoo Han <78309388+hanuuuuU@users.noreply.github.com>
2026-06-04 23:44:15 +0900

    [Passed] 특정 구간의 원소 평균값 (77ms, 17MB)

---
+++ trail2/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+cnt=0
+for i in range(n):
+    for j in range(i,n):
+        if sum(arr[i:j+1])/(j-i+1) in arr[i:j+1]:
+            cnt+=1
+print(cnt)
